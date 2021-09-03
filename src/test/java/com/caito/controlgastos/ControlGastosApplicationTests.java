3bfe251167f3a8ede238bd239b5b981187a0cb7b
caito <caitocd@gmail.com>
2021-09-03 14:59:44 -0300

    first commit

---
+++ src/test/java/com/caito/controlgastos/ControlGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caito.controlgastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
